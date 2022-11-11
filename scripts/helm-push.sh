9fb7c23f83ae584c4272e47a34b34c8d91139ee7
alex-treebeard <alex@treebeard.io>
2022-11-11 12:19:19 +0000

    push

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 set -eu -x
+
+version=0.3.0-tbt-"$(git rev-parse --short HEAD)"
+
 helm package \
+  --version "${version}" \
   ./helm-chart/kuberay-apiserver \
   ./helm-chart/kuberay-operator \
   ./helm-chart/ray-cluster
   
-# helm push kuberay-operator-0.3.0.tgz oci://ghcr.io/treebeardtech/kuberay/helm-chart
-# helm push kuberay-apiserver-0.3.0.tgz oci://ghcr.io/treebeardtech/kuberay/helm-chart
-# helm push kuberay-operator-0.3.0.tgz oci://ghcr.io/treebeardtech/kuberay/helm-chart
+helm push kuberay-operator-"${version}".tgz oci://ghcr.io/treebeardtech/kuberay/helm-chart
+helm push kuberay-apiserver-"${version}".tgz oci://ghcr.io/treebeardtech/kuberay/helm-chart
+helm push ray-cluster-"${version}".tgz oci://ghcr.io/treebeardtech/kuberay/helm-chart
